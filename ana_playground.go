85f59a56d068454ef7e99a4f1e95ff3d2b5d5c56
sinmetal <sinmetal@sinmetalcraft.jp>
2020-06-29 09:58:06 +0900

    initial commit

---
+++ ana_playground.go
@@ -0,0 +1,40 @@
+package ana_playground
+
+import (
+	"go/ast"
+
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/ast/inspector"
+)
+
+const doc = "ana_playground is ..."
+
+// Analyzer is ...
+var Analyzer = &analysis.Analyzer{
+	Name: "ana_playground",
+	Doc:  doc,
+	Run:  run,
+	Requires: []*analysis.Analyzer{
+		inspect.Analyzer,
+	},
+}
+
+func run(pass *analysis.Pass) (interface{}, error) {
+	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+
+	nodeFilter := []ast.Node{
+		(*ast.Ident)(nil),
+	}
+
+	inspect.Preorder(nodeFilter, func(n ast.Node) {
+		switch n := n.(type) {
+		case *ast.Ident:
+			if n.Name == "gopher" {
+				pass.Reportf(n.Pos(), "identifyer is gopher")
+			}
+		}
+	})
+
+	return nil, nil
+}
