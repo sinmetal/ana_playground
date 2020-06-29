99446129eb58c910e5c5c78c486411d3b95d5e57
sinmetal <sinmetal@sinmetalcraft.jp>
2020-06-29 10:12:18 +0900

    test通るようにしてみた

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -2,6 +2,6 @@ package a
 
 func f() {
 	// The pattern can be written in regular expression.
-	var gopher int // want "pattern"
+	var gopher int // want "identifyer is gopher"
 	print(gopher)  // want "identifyer is gopher"
 }
